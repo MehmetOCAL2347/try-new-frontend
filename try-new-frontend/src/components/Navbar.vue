3edb57dbe527b5ec82b62e50393847359ff10483
MehmetOCAL2347 <ocal.mehmet@outlook.com.tr>
2021-12-05 00:40:55 +0300

    Navbar Changed

--- try-new-frontend/src/components/Navbar.vue
+++ try-new-frontend/src/components/Navbar.vue
@@ -14,35 +14,6 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
         <form class="d-flex">
           <input
             class="form-control me-2"
@@ -52,17 +23,33 @@
           />
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Kurslar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Kitaplar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"
+              >Çalışma Takvimi</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Destek</a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
 </template>
 <script>
 export default {
-    name:'Navbar',
-    props: {
-
-    }
+  name: "Navbar",
+  props: {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
